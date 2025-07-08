625a39ae9eb63740c9dac0bcb22f5342dab70199
Janet Tong <janet.pypy@gmail.com>
2025-07-08 21:29:55 +0800

    Try Push

--- ManningAutoTest/src/test/java/MainTest.java
+++ ManningAutoTest/src/test/java/MainTest.java
@@ -4,7 +4,7 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-
+//Try Push
 public class MainTest {
     @Test
     public void Main() throws InterruptedException {
