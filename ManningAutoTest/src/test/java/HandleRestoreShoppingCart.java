2f8b48892023a537510a18edab8bf9e418f8bb5e
Janet Tong <janet.pypy@gmail.com>
2025-07-10 21:39:40 +0800

    // Add HandleCaptcha // Add HandleRestoreShoppingCart // Add LoginTest

---
+++ ManningAutoTest/src/test/java/HandleRestoreShoppingCart.java
@@ -0,0 +1,28 @@
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.options.AriaRole;
+
+public class HandleRestoreShoppingCart {
+    public static void restoreShoppingCart(Page page) {
+        System.out.println("In Restore Shopping Cart");
+
+        // Default to restore the shopping cart
+        // Set restore to false if you do not want to restore the shopping cart
+        boolean restore = true;
+
+        if (restore) {
+            Locator restoreYesBtn = page.getByRole(AriaRole.BUTTON,
+                    new Page.GetByRoleOptions().setName("Yes"));
+            if (restoreYesBtn.isVisible()) {
+                restoreYesBtn.click();
+            }
+        } else {
+            Locator restoreNoBtn = page.getByRole(AriaRole.BUTTON,
+                    new Page.GetByRoleOptions().setName("No").setExact(true));
+            if (restoreNoBtn.isVisible()) {
+                restoreNoBtn.click();
+
+            }
+        }
+    }
+}
