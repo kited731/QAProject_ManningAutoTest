c1ce2ff3f575270f88a3e09291756675fd134d8d
Jason Wong <wongcjason12@gmail.com>
2025-07-12 14:31:28 +0800

    Added Faker to ClickNCollectTest.java

--- ManningAutoTest/src/test/java/ClickNCollectTest.java
+++ ManningAutoTest/src/test/java/ClickNCollectTest.java
@@ -1,5 +1,7 @@
+
 import org.junit.jupiter.api.Test;
 
+import com.github.javafaker.Faker;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Locator;
@@ -13,6 +15,42 @@ public class ClickNCollectTest {
     public void testManningsThroatWesternPage() { // <-- Make method public
         Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(2000));
         Page page = browser.newPage();
+
+        Faker faker = new Faker();
+        String email = faker.name().firstName() + "_" + faker.name().lastName() + "@gmail.com";
+        String fullName = faker.name().firstName() + " " + faker.name().lastName();
+        int regionCode = faker.number().numberBetween(0, 2);
+        String region = "Kowloon";
+        String district = faker.address().city();
+        String streetNumber = faker.address().streetAddress();
+        String buildingName = faker.address().buildingNumber();
+        String floor = faker.number().randomDigitNotZero() + "/F";
+        String countryCode = "+852";
+        String mobile = "61235123";
+
+        switch (regionCode) {
+            case 0:
+                region = "Hong Kong Island";
+                break;
+            case 1:
+                region = "Kowloon";
+                break;
+            case 2:
+                region = "New Territories";
+                break;
+            default:
+                region = "Hong Kong Island";
+                break;
+        }
+        System.out.println("email is : " + email);
+        System.out.println("Full Name is : " + fullName);
+        System.out.println("Region is : " + region);
+        System.out.println("District is : " + district);
+        System.out.println("Street Number is : " + streetNumber);
+        System.out.println("Building is : " + buildingName);
+        System.out.println("Floor is : " + floor);
+        System.out.println("Country Code is : " + countryCode);
+        System.out.println("Mobile is : " + mobile);
 
         try (Playwright playwright = Playwright.create()) {
             page.navigate("https://www.mannings.com.hk/en/strepsils-sugarfree-lemon-lozenge-16pcs/p/492595");
@@ -28,16 +66,15 @@ public class ClickNCollectTest {
             page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter location")).fill("Lai Chi Kok");
             page.locator(".storeCollect-radioGroup-hz8 > .icon-root-W-v > svg").first().click();
             page.locator("input[name=\"firstName\"]").click();
-            page.locator("input[name=\"firstName\"]").fill("John Doe");
+            page.locator("input[name=\"firstName\"]").fill(fullName);
             page.locator("form").filter(new Locator.FilterOptions().setHasText("Full NameEmailCountry")).locator("input[name=\"email\"]").click();
-            page.locator("form").filter(new Locator.FilterOptions().setHasText("Full NameEmailCountry")).locator("input[name=\"email\"]").fill("abc@gmail.com");
+            page.locator("form").filter(new Locator.FilterOptions().setHasText("Full NameEmailCountry")).locator("input[name=\"email\"]").fill(email);
             page.locator("#areaCode div").nth(4).click();
             page.getByRole(AriaRole.OPTION, new Page.GetByRoleOptions().setName("+852")).click();
             page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Mobile number")).click();
-            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Mobile number")).fill("61236123");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Mobile number")).fill(mobile);
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Confirm Details")).click();
 
-            page.pause();
             browser.close();
         }
     }
