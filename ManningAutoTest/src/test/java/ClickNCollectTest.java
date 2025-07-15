b9099781cb1ffce926e919559f7557b7efe3adfd
Jason Wong <wongcjason12@gmail.com>
2025-07-15 21:00:12 +0800

    Improve ClickNCollectTest.java code flexibility

--- ManningAutoTest/src/test/java/ClickNCollectTest.java
+++ ManningAutoTest/src/test/java/ClickNCollectTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.jupiter.api.Test;
 
 import com.github.javafaker.Faker;
@@ -25,7 +26,7 @@ public class ClickNCollectTest {
         String buildingName = faker.address().buildingNumber();
         String floor = faker.number().randomDigitNotZero() + "/F";
         String countryCode = "+852";
-        String mobile = "61235123";
+        String mobile = "61236123";
 
         System.out.println("email is : " + email);
         System.out.println("Full Name is : " + fullName);
@@ -43,7 +44,7 @@ public class ClickNCollectTest {
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to cart")).nth(0).click();
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Increase Quantity")).click();
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Toggle mini cart. You have 2")).click();
-            page.locator(".deliveryMethod-item_content--Fx > div:nth-child(3) > .icon-root-W-v > svg").first().click();
+            page.locator(".deliveryMethod-item_content--Fx > div:nth-child(3) > .icon-root-W-v > svg").first().click(); //select Click and collect
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Increase Quantity")).click();
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Checkout securely")).click();
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Guest checkout")).click();
@@ -62,5 +63,51 @@ public class ClickNCollectTest {
 
             browser.close();
         }
+    }
+
+// improved code starts here
+    public static void AddToCart(Page page) {
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("I understand")).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to cart")).nth(0).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Increase Quantity")).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Toggle mini cart. You have 2")).click();
+    }
+
+    public static void selectClickNCollect(Page page) {
+        page.locator(".deliveryMethod-item_content--Fx > div:nth-child(3) > .icon-root-W-v > svg").first().click();
+    }
+
+    public static void increaseQtyNCheckoutSecurely(Page page) {
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Increase Quantity")).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Checkout securely")).click();
+    }
+
+    public static void GuestCheckout(Page page) {
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Guest checkout")).click();
+    }
+
+    public static void InputCheckOutInfo(Page page, String email, String fullname, String mobile) {
+        // Faker faker = new Faker();
+        //   String fullName = faker.name().firstName() + " " + faker.name().lastName();
+        // int regionCode = faker.number().numberBetween(0, 2);
+        // String region = "Kowloon";
+        // String district = faker.address().city();
+        // String streetNumber = faker.address().streetAddress();
+        // String buildingName = faker.address().buildingNumber();
+        // String floor = faker.number().randomDigitNotZero() + "/F";
+        // String countryCode = "+852";
+
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter location")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter location")).fill("Lai Chi Kok");
+        page.locator(".storeCollect-radioGroup-hz8 > .icon-root-W-v > svg").first().click();
+        page.locator("input[name=\"firstName\"]").click();
+        page.locator("input[name=\"firstName\"]").fill(fullname);
+        page.locator("form").filter(new Locator.FilterOptions().setHasText("Full NameEmailCountry")).locator("input[name=\"email\"]").click();
+        page.locator("form").filter(new Locator.FilterOptions().setHasText("Full NameEmailCountry")).locator("input[name=\"email\"]").fill(email);
+        page.locator("#areaCode div").nth(4).click();
+        page.getByRole(AriaRole.OPTION, new Page.GetByRoleOptions().setName("+852")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Mobile number")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Mobile number")).fill(mobile);
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Confirm Details")).click();
     }
 }
